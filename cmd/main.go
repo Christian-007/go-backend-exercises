35cd80096f355aced21cf26fc78b6844ff4806e7
Christian-007 <christianingsunardi@gmail.com>
2025-12-18 22:08:31 +0700

    Add solution to 01-base-http-server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	server := &http.Server{
+		Addr:    ":4000",
+		Handler: Routes(logger),
+	}
+
+	logger.Info("starting server", "addr", ":4000")
+	err := server.ListenAndServe()
+	if err != nil {
+		logger.Error(err.Error())
+		panic(err)
+	}
+}
+
+func Routes(logger *slog.Logger) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		response := map[string]string{"message": "Ok"}
+
+		w.Header().Set("Content-Type", "applications/json")
+		w.WriteHeader(http.StatusOK)
+
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			logger.Error("failed to encode a JSON response", slog.String("error", err.Error()))
+			http.Error(w, "failed to encode response", http.StatusInternalServerError)
+		}
+	})
+
+	return r
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-backend-exercises
 
 go 1.23.3
+
+require github.com/go-chi/chi v1.5.5
